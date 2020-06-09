3e4d9ae5bc644d82e816296c5d259840c7e3758b
Terris Kremer <terris.kremer@gmail.com>
2020-06-09 16:42:41 -0400

    add basic flash styling

---
+++ api/src/graphql/users.sdl.js
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag'
+
+export const schema = gql`
+  type User {
+    id: Int!
+    email: String!
+    name: String
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: Int!): User!
+  }
+
+  input CreateUserInput {
+    email: String!
+    name: String
+  }
+
+  input UpdateUserInput {
+    email: String
+    name: String
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
+    updateUser(id: Int!, input: UpdateUserInput!): User!
+    deleteUser(id: Int!): User!
+  }
+`
